96948ba7d436ece1d8ef0e19b6cfffa55e352057
Salman Ahmed <alsedeeq6@gmail.com>
2025-03-12 08:32:23 +0300

    complete main tasks

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,8 +2,15 @@
     require_once 'includes/header.php';
     require_once 'includes/navbar.php';
     require_once 'includes/user.php';
-    require_once 'content.php';
 ?>
 
+<script>
+    if (window.innerWidth < 768) {
+        window.location.href = 'small_screen.php';
+    }
+</script>
+
 <?php
+    require_once 'content.php';
     require_once 'includes/footer.php';
+?>
